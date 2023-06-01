4183cb6e46991c9d6d5dd223582981d0fb5696d7
Kemirdin Kaynam <Kemirdin.Kaynam@salesmanager.nl>
2023-06-01 11:27:17 +0200

    Add project files.

---
+++ DapperCrudTutorial/Controllers/SuperHeroController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperCrudTutorial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public SuperHeroController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult>
+
+    }
+}
